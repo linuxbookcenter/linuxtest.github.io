de6efb2d616fb9ac68a7673749bdb19e7d61fd3e
linuxbookcenter <62329924+linuxbookcenter@users.noreply.github.com>
2020-03-21 20:00:59 +0530

    dd

--- data/Excavated_Area_6.js
+++
@@ -1,9 +0,0 @@
-var json_Excavated_Area_6 = {
-"type": "FeatureCollection",
-"name": "Excavated_Area_6",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Name": "Zone 1", "PriorityLe": "Medium Priority", "Status": "To be Decide", "District": "Thoothukudi", "Village": "\", "ELEVATION": "0" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 77.869720, 8.620616 ] ] ] } },
-
-]
-}
