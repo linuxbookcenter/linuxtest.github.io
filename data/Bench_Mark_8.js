8757495dbaebd16971709c369b6a92320f1ab19d
linuxbookcenter <62329924+linuxbookcenter@users.noreply.github.com>
2020-03-21 18:28:54 +0530

    deleting

--- data/Bench_Mark_8.js
+++
@@ -1,9 +0,0 @@
-var json_Bench_Mark_8 = {
-"type": "FeatureCollection",
-"name": "Bench_Mark_8",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "NAME": "Bench Mark" }, "geometry": { "type": "Point", "coordinates": [ 77.523072382149977, 11.108534284670032 ] } },
-{ "type": "Feature", "properties": { "NAME": "Bench Mark" }, "geometry": { "type": "Point", "coordinates": [ 77.522216278058792, 11.110177019524713 ] } }
-]
-}
